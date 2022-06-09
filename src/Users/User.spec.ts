d31ff8b1a03d58bba317f6041d04592ae80a43f8
JoaoPaulo <joaopaulosg17@gmail.com>
2022-06-09 18:01:07 -0300

    Adicionadas dependencias e testes para as rotas de users

---
+++ src/Users/User.spec.ts
@@ -0,0 +1,20 @@
+import request from "supertest";
+import { app } from "../app"
+
+describe("Testes das rotas de users",()=>{
+    test("Teste de adicionar user",async()=>{
+        const response = await request(app)
+        .post("/users/new")
+        .send({name:"testesJest",password:"123",email:"testeJest@email.com"});
+        expect(response.body.msg).toEqual("Usuario criado");
+        expect(response.statusCode).toEqual(201);
+    });
+    test("Teste de login",async()=>{
+        const response = await request(app)
+        .post("/users/login")
+        .send({email:"teste3",password:"123"});
+        console.log(response)
+        expect(response.statusCode).toEqual(200);
+        expect(response.body.msg.length).toBeGreaterThan(0);
+    })
+})
