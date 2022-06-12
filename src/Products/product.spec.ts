4b1ddc0bda479492c77f82be8ca780e712354d63
JoaoPaulo <joaopaulosg17@gmail.com>
2022-06-12 08:13:43 -0300

    Adicionado novo teste para a rota de products

--- src/Products/product.spec.ts
+++ src/Products/product.spec.ts
@@ -8,5 +8,13 @@ describe("Testes das rotas de products",()=>{
         .send({name:"testeJest",price:20.0})
         expect(response.statusCode).toEqual(201);
         expect(response.body.msg).toEqual("Produto cadastrado");
-    })
+    });
+
+    test("Teste de adicionar product sem todos os campos",async()=>{
+        const response = await request(app)
+        .post("/products/new")
+        .send({name:"testeJest"});
+        expect(response.statusCode).toEqual(500);
+        expect(response.body.msg).toEqual("Produto não possui todos os dados");
+    });
 })
