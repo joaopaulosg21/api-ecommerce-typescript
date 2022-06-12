b8bb7a19dffb744532dde6e2217d3a56a8cb227d
JoaoPaulo <joaopaulosg17@gmail.com>
2022-06-12 08:37:23 -0300

    Adicionados novos testes para as rotas de cart

--- src/Cart/cart.spec.ts
+++ src/Cart/cart.spec.ts
@@ -12,10 +12,36 @@ describe("Testes das rotas de cart",()=>{
     test("Teste de ver cart",async()=>{
         const response = await request(app)
         .get("/cart/")
-        .set({"authorization":"Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MSwiaWF0IjoxNjU0NzgxMjEyfQ.keNgW4c__XPooeZZCfAdc16f_EedaX2MDuxQjGxtzI8"})
-        expect(response.body.msg).toBeInstanceOf(Object);
+        .set({"authorization":"Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MjUsImlhdCI6MTY1NTAzMjc2MX0.C03ZXHbxgjbdcSdfkjvXPLDh5GRvNKGVx8evi0BazxI"});
+        expect(response.body.msg.id).toBeGreaterThan(0);
         expect(response.body.msg.items.length).toBeGreaterThan(0);
         expect(response.statusCode).toEqual(200);
 
-    })
+    });
+
+    test("Teste para ver carrinho que não existe",async()=>{
+        const response = await request(app)
+        .get("/cart/")
+        .set({"authorization":"Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MjYsImlhdCI6MTY1NTAzMzYxOX0.Uwtl3owQIkrkJviFbm4LRMHELWXLB0QQJxrgZCPnLY8"});
+        expect(response.body.msg).toEqual("Carrinho não existe");
+        expect(response.statusCode).toEqual(404);
+
+     })
+
+    test("Teste para criar novo cart",async()=>{
+        const response = await request(app)
+        .post("/cart/addItem/4")
+        .set({"authorization":"Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MjUsImlhdCI6MTY1NTAzMjc2MX0.C03ZXHbxgjbdcSdfkjvXPLDh5GRvNKGVx8evi0BazxI"});
+        expect(response.body.msg.id).toBeGreaterThan(0);
+        expect(response.body.msg.items.length).toBeGreaterThan(0);
+        expect(response.statusCode).toEqual(200);
+    });
+
+    test("Teste para adicionar product que não existe no cart",async()=>{
+        const response = await request(app)
+        .post("/cart/addItem/1")
+        .set({"authorization":"Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MjUsImlhdCI6MTY1NTAzMjc2MX0.C03ZXHbxgjbdcSdfkjvXPLDh5GRvNKGVx8evi0BazxI"});
+        expect(response.body.msg).toEqual("Produto não existe");
+        expect(response.statusCode).toEqual(404);
+    });
 })
